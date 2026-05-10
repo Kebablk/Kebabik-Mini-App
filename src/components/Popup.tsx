801cf80661aeed81e3b7d8d3f0f27740333cc56f
Kebablk <glbuhmaterik2010@gmail.com>
2026-05-10 19:09:23 +0300

    add transitions

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,20 +2,43 @@ import { useEffect, useState } from "react";
 
 export default function Popup() {
   const [showPopup, setShowPopup] = useState<boolean>(true);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const [isVisibleText, setIsVisibleText] = useState<boolean>(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowPopup(false);
-    }, 5000);
+    const appearanceTimer = setTimeout(() => {
+      setIsVisibleText(true);
+    }, 500);
 
-    return () => clearTimeout(timer);
+    const hideTimer = setTimeout(() => {
+      setIsVisible(false);
+      setIsVisibleText(false);
+    }, 3000);
+
+    const popupTimer = setTimeout(() => {
+      setShowPopup(false);
+    }, 4000);
+
+    return () => {
+      clearTimeout(appearanceTimer);
+      clearTimeout(popupTimer);
+      clearTimeout(hideTimer);
+    };
   }, []);
+
+  useEffect(() => {}, []);
 
   if (!showPopup) return null;
 
   return (
-    <div className="w-full h-dvh bg-[#131215] flex justify-center items-center">
-      <h1 className="text-white text-6xl font-semibold">Wassup!</h1>
+    <div
+      className={`w-full h-dvh bg-[#131215] opacity-100 flex justify-center items-center transition-all duration-1000 z-50${!isVisible && "opacity-0"}`}
+    >
+      <h1
+        className={`text-white text-[50px] font-semibold transition-all duration-1000 opacity-0 ${isVisibleText && "opacity-100"}`}
+      >
+        Wassup!
+      </h1>
     </div>
   );
 }
